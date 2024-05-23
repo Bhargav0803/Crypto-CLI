68a8fe08a1834fe2f9952c590b9cb32b10e9e847
Bhargav0803 <bhargavmungekar08@gmail.com>
2024-05-23 13:36:11 +0530

    added api call

--- commands/view.js
+++ commands/view.js
@@ -1,7 +1,19 @@
+import KeyManager from "../lib/KeyManager.js"
+import CryptoAPI from "../lib/CryptoAPI.js";
+
 const view = {
-    price(cmd) {
+    async price(cmd) {
         // console.log("HEllo")
-        
+        try {
+            const keyManager = new KeyManager();
+            const key = keyManager.getKey();
+            const api = new CryptoAPI();
+            const priceData = await api.getPriceData(cmd.coin);
+            console.log(priceData);
+
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
 
